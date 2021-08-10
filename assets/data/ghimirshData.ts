e6e81d239a19131ee38fe5ce4d44242dc2386182
Shankar Ghimire <34399132+shankarghimire@users.noreply.github.com>
2021-08-10 19:28:56 -0400

    Add files via upload

---
+++ assets/data/ghimirshData.ts
@@ -0,0 +1,9 @@
+import { Ghimirsh } from "src/app/type";
+
+export const GHIMIRSHDATA:Ghimirsh={
+    studentNumber:991585190,
+    name:'Shankar Ghimire',
+    loginName: 'ghimirsh',
+    campusName: 'Davis Campus',
+    assignmentTitle: 'Assignment-04'
+}
